23f3e995faead5b1f2e3b9e125e83db4d55947cb
YIHYUN HA <hihyun16@gmail.com>
2022-07-02 14:52:55 +0900

    Refactoring server code - bin/www 삭제 - var를 const로 변경

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const PORT = 3000;
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,4 +38,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+    console.log(`server listen http://localhost:${PORT}`);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var axios = require('axios');
-var router = express.Router();
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
 
 router.get('/', async function(req, res) {
     const url = req.query.url;

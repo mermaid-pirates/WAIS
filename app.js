daf0a7a424153d4f7aadbfbd093af1b14b9b6778
YIHYUN HA <hihyun16@gmail.com>
2022-07-02 14:56:16 +0900

    Add styleRouter

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
+const styleRouter = require('./routes/style');
 
 const PORT = 3000;
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/style', styleRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

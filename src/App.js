593e86367a72ed398e6c05a2ac54ea8348819a10
NoJe <nojy99wodnjs@gmail.com>
2022-07-02 13:26:49 +0900

    second prototype for project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import Navbar from "./nav/nav";
-import './css/App.css';
+import './App.css';
+import { useState } from "react";
+import Navbar from './nav';
+import RenderPage from './RenderPage';
 
-function App() {
-  const nav = <Navbar />
+
+function App(props) {
+  const [styleSheet, setStyle] = useState({});
+  const [page_url, setPage] = useState(props.page_url);
+  const event_controller = (e, func)=>{
+    if(func === "show_origin_page"){
+      console.log("show_origin_page");
+    } else if(func === "restrict_blick"){
+      console.log("restrict_blick");
+    } else if(func === "contrast_color_up"){
+      console.log("contrast_color_up");
+    } else if(func === "contrast_color_down"){
+      console.log("contrast_color_down");
+    } else if(func === "contrast_brightness_up"){
+      console.log("contrast_brightness_up");
+    } else if(func === "contrast_brightness_down"){
+      console.log("contrast_brightness_down");
+    } else if(func === "font_sizing_up"){
+      console.log("font_sizing_up");
+    } else if(func === "font_sizing_down"){
+      console.log("font_sizing_down");
+    }
+  }
+
+  const nav = <Navbar e={event_controller}/>
+  const render_page = <RenderPage src={props.page_url} sheet={styleSheet}/>
 
   return (
     <div className="App">
       {nav}
-      <iframe
-        src="https://naver.com"
-        name="프레임 이름"
-        width="100%"
-        height="575px"
-        sandbox="allow-scripts allow-popups">
-        iframe를 지원하지 않는 브라우저인 경우 대체정보를 제공  
-      </iframe>
+      {render_page}
     </div>
   );
 }
--- src/ToggleSwitch.js
+++ src/ToggleSwitch.js
@@ -1,12 +1,15 @@
-import '../../css/ToggleSwitch.css'
+import './ToggleSwitch.css'
 
 function ToggleSwitch(props){
+    const toggle_e = (e)=>{
+        props.e(e, props.func);
+    }
 
     return (
         <div className="toggle-sw-box">
             <h3 className='label'>{props.label}</h3>
             <label className="switch">
-                <input type="checkbox" />
+                <input type="checkbox" onClick={toggle_e}/>
                 <span className="slider round"></span>
             </label>
         </div>

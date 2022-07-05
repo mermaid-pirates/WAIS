d04cf4a3ca0afc5fce0606f2d049509b8faeec57
YIHYUN HA <hihyun16@gmail.com>
2022-07-05 15:56:55 +0900

    Modify port / Add cors middleware - 클라이언트와의 포트 충돌에 의해 포트 번호를 3000에서 4000으로 변경 - 클라이언트와의 ajax 통신을 위한 cors 미들웨어 추가

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const styleRouter = require('./routes/style');
 
-const PORT = 3000;
+const PORT = 4000;
 
 const app = express();
 
@@ -20,6 +21,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
 
 app.use('/', indexRouter);
 app.use('/style', styleRouter);

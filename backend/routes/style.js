b8b23c60ce5c3e2eae04bf3852a3aea495f25c49
Yeonji-Lim <view7186@naver.com>
2022-07-06 21:30:35 +0900

    Refactoring for future change

--- backend/routes/style.js
+++ backend/routes/style.js
@@ -16,10 +16,9 @@ router.get('/dark', (req, res) => {
 
 router.post('/dark', (req, res) => {
     var style = '';
-    var context;
     var style_list = req.body.style_change;
-    if(style_list === 'dark') style = dark.getDarkModeStyle();
-    context = {result:style}
+    if(style_list === 'dark') style += dark.getDarkModeStyle();
+    var context = {result:style}
     res.writeHead('200', { 'Content-Type': 'text/html; charset=utf8' });
     req.app.render('style-test', context, function (err, html) { // pug의 #{result}에 스타일 시트 내용 삽입
         res.end(html);

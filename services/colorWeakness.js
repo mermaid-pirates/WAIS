8e73de767ce9dff5135f089fe6152a4572a52503
Yeonji-Lim <view7186@naver.com>
2022-07-07 19:57:24 +0900

    Seperate functions - getStyle : CSS 내용 반환 - getRenderedHtml : 렌더링된 html 반환

--- services/colorWeakness.js
+++ services/colorWeakness.js
@@ -1,6 +1,30 @@
 const color_converter = require('./getColor');
 
-const getStyle = (html_data) => {
+const getStyle = () => {
+    const style = [];
+    style.push(`html, body {`);
+    style.push(`    background-color: #ffffff;`);
+    style.push(`    color: #000000;`);
+    style.push('}');
+    style.push('a {');
+    style.push(`    color: #0072b1;`);
+    style.push('}');
+    style.push('table {');
+    style.push(`    border-color: #808080;`);
+    style.push('}');
+    style.push('::placeholder {');
+    style.push(`    color: #a9a9a9;`);
+    style.push('}');
+    style.push('input:-webkit-autofill,');
+    style.push('textarea:-webkit-autofill,');
+    style.push('select:-webkit-autofill {');
+    style.push(`    background-color: #f0e542 !important;`);
+    style.push(`    color: white !important;`);
+    style.push('}');
+    return style.join('\n');
+}
+
+const getRenderedHtml = (html_data) => {
     var pattern = /#[\da-f]{6}/gi;
     if(pattern.test(html_data)) {
         var target_list = [...new Set(html_data.match(pattern))];
@@ -11,4 +35,4 @@ const getStyle = (html_data) => {
     return html_data;
 }
 
-module.exports = { getStyle };
+module.exports = { getStyle, getRenderedHtml };

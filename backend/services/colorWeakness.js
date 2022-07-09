77b4b330a037253958a59b0dd71b350bfc7d435c
Yeonji-Lim <view7186@naver.com>
2022-07-09 15:31:18 +0900

    Delete unused selector

--- backend/services/colorWeakness.js
+++ backend/services/colorWeakness.js
@@ -9,17 +9,8 @@ const getStyle = () => {
     style.push('a {');
     style.push(`    color: #0072b1;`);
     style.push('}');
-    style.push('table {');
-    style.push(`    border-color: #808080;`);
-    style.push('}');
     style.push('::placeholder {');
     style.push(`    color: #a9a9a9;`);
-    style.push('}');
-    style.push('input:-webkit-autofill,');
-    style.push('textarea:-webkit-autofill,');
-    style.push('select:-webkit-autofill {');
-    style.push(`    background-color: #f0e542 !important;`);
-    style.push(`    color: white !important;`);
     style.push('}');
     return style.join('\n');
 }

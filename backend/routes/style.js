bf4a03eee6e7dad9cccd7828b5590747e90e04d7
Yeonji-Lim <view7186@naver.com>
2022-07-09 15:37:52 +0900

    Get example html data from file

--- backend/routes/style.js
+++ backend/routes/style.js
@@ -1,59 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 const dark = require('../services/dark');
 const weak = require('../services/colorWeakness');
 const cont = require('../services/highContrast');
 const CSSadder = require('../services/addStyleSheet');
-
-const example_html_data = `<html><head>
-    <title>Example Domain</title>
-
-    <meta charset="utf-8">
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style type="text/css">
-    body {
-        background-color: #f0f0f2;
-        margin: 0;
-        padding: 0;
-        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;   
-    }
-    div {
-        width: 600px;
-        margin: 5em auto;
-        padding: 2em;
-        background-color: #fdfdff;
-        border-radius: 0.5em;
-        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);
-    }
-    a:link, a:visited {
-        color: #38488f;
-        text-decoration: none;
-    }
-    @media (max-width: 700px) {
-        div {
-            margin: 0 auto;
-            width: auto;
-        }
-    }
-    </style>    
-<script src="chrome-extension://mooikfkahbdckldjjndioackbalphokd/assets/prompt.js"></script></head>
-
-<body>
-<div style="background-color:#123456">
-    <h1 style="color:#005caf;">Custom Example Domain</h1>
-    <p>This domain is for use in illustrative examples in documents. You may use this
-    domain in literature without prior coordination or asking for permission.</p>
-    <p><a href="https://www.iana.org/domains/example">More information...</a></p>
-</div>
-<div style="background-color:#247833;">
-    <h1 style="color:#5caf00;">Custom Example Domain</h1>
-    <p>This domain is for use in illustrative examples in documents. You may use this
-    domain in literature without prior coordination or asking for permission.</p>
-    <p><a href="https://www.iana.org/domains/example">More information...</a></p>
-</div>
-
-</body></html>`;
+const example_html_data = fs.readFileSync('data/sampleHTML', 'utf8');
 
 // TODO: 색상 변경 모드 API
 

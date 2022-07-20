81deff0e4d48d1ddf226e05db3d2352c5c7a70e5
Yeonji-Lim <view7186@naver.com>
2022-07-20 17:13:42 +0900

    Rename file, Add delete Stylesheet service, Refactoring

--- backend/services/addStyleSheet.js
+++
@@ -1,6 +0,0 @@
-const setStyle = (html, style, style_name) => {
-    html = html.slice(0, html.search('</head>')) +'<style class="wais '+style_name+'">'+style +'</style>'+ html.slice(html.search('</head>'));
-    return html;
-}
-
-module.exports = { setStyle };

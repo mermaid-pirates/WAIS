386b072352e4d5787c264c398c138c7f0680f029
YIHYUN HA <hihyun16@gmail.com>
2022-07-22 23:37:04 +0900

    Refactoring modifyLink logic - 매칭되는 경로 선정 알고리즘 변경

--- backend/services/modifyLink.js
+++ backend/services/modifyLink.js
@@ -1,23 +1,16 @@
 const CLIENT_URL = 'http://localhost:3000';
 const SEARCH_URL = CLIENT_URL + '/searching?search=';
 
-const ignore_extension = [
-    'png',
-    'jpg',
-    'jpeg',
-    'css',
-    'gif',
-    'svg',
-    'js',
-    'ico',
-];
-
 const modifyLink = (url, html) => {
     const context_path = getContextPath(url);
-    const modified_relative_html = getRelativePath(context_path, html);
-    const modified_absolute_html = getAbsolutePath(modified_relative_html);
 
-    return modified_absolute_html;
+    const a_tag_regex = /<a [^>]*>?/g;
+    let match_list = [...new Set(html.match(a_tag_regex))];
+    let replaced_match_list = [...new Set(html.match(a_tag_regex))];
+    replaced_match_list = getRelativePath(replaced_match_list, context_path);
+    replaced_match_list = getAbsolutePath(replaced_match_list);
+
+    return getReplacedHtml(html, match_list, replaced_match_list);
 }
 
 // input parameter: https://example.com/abc/def
@@ -34,48 +27,19 @@ const getContextPath = (url) => {
     return null;
 }
 
-const getRelativePath = (context_path, html) => {
-    const relative_path_regex = /href="\/[a-z][^\\\\\:\|\<\>\"\*\?]*/gi; // 상대 경로 정규식
-    let match_list = [...new Set(html.match(relative_path_regex))];
-    if (!match_list) return html;
-    match_list = match_list.map(e => e.replace('href="', ''));
-
-    match_list = match_list.filter((url) => {
-        const split_data = url.split('.');
-        const extension = split_data[split_data.length-1];
-        if (ignore_extension.indexOf(extension) == -1) {
-            return true;
-        }
-    });
-    
-    let new_html = html;
-    match_list.forEach((url) => {
-        new_html = new_html.replace(url, context_path + url);
-    });
-
-    return new_html;
+const getRelativePath = (match_list, context_path) => {
+    return match_list.map(e => e.replace('href="/', 'href="' + context_path + "/"));
 }
 
-const getAbsolutePath = (html) => {
-    const url_regex = /href="https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi; // URL 정규식
-    let match_list = [...new Set(html.match(url_regex))];
-    if (!match_list) return html;
-    match_list = match_list.map(e => e.replace('href="', ''));
+const getAbsolutePath = (match_list) => {
+    return match_list.map(e => e.replace('href="http', 'href="' + SEARCH_URL + "http"))
+}
 
-    match_list = match_list.filter((url) => {
-        if (url.indexOf(SEARCH_URL) > -1) return false;
-        const split_data = url.split('.');
-        const extension = split_data[split_data.length-1];
-        if (ignore_extension.indexOf(extension) == -1) {
-            return true;
-        }
+const getReplacedHtml = (html, match_list, replaced_match_list) => {
+    match_list.forEach((value, index) => {
+        html = html.replace(value, replaced_match_list[index]);
     });
-    
-    let new_html = html;
-    match_list.forEach((url) => {
-        new_html = new_html.replace(url, SEARCH_URL + url);
-    });
-    return new_html;
+    return html;
 }
 
 module.exports = modifyLink;

0195614f1e9c9baf370e4b84dd0706a2d5e2b631
Yeonji-Lim <view7186@naver.com>
2022-07-23 10:57:48 +0900

    Rename files & Modify color mode api - color mode에 관련된 파일들은 color로 시작하는 이름으로 변경 - 바뀐 ManageStyleSheet.js에 따라 내용 변경

---
+++ backend/services/colorHighContrast.js
@@ -0,0 +1,13 @@
+const strict_mode = true;
+const setting = {
+    default_color: 'white',
+    light_color: '#bfbfbf',
+    link_color: '#f0e542',
+    default_background: 'black',
+    light_background: 'grey',
+    default_border: '#009f73',
+    default_placeholder: 'white'
+};
+const getRenderedHtml = (html_data) => { return html_data; }
+
+module.exports = { strict_mode, setting, getRenderedHtml };
--- backend/services/colorWeakness.js
+++ backend/services/colorWeakness.js
@@ -1,19 +1,6 @@
-const weak_color = require('./getColor');
-
-const getStyle = () => {
-    const style = [];
-    style.push(`#body html, body {`);
-    style.push(`    background-color: #ffffff;`);
-    style.push(`    color: #000000;`);
-    style.push('}');
-    style.push('#body a {');
-    style.push(`    color: #0072b1;`);
-    style.push('}');
-    style.push('#body ::placeholder {');
-    style.push(`    color: #a9a9a9;`);
-    style.push('}');
-    return style.join('\n');
-}
+const weak_color = require('./getWeakColor');
+const strict_mode = false;
+const setting = {};
 
 const getRenderedHtml = (html_data) => {
     const pattern = /#[\da-f]{6}/gi;
@@ -26,4 +13,4 @@ const getRenderedHtml = (html_data) => {
     return html_data;
 }
 
-module.exports = { getStyle, getRenderedHtml };
+module.exports = { strict_mode, setting, getRenderedHtml };

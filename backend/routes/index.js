b8387d93f17ea2ff0d4a27f5c655fbb1757dd4e2
YIHYUN HA <hihyun16@gmail.com>
2022-07-23 17:05:38 +0900

    Add try-catch to index router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,25 +6,28 @@ const router = express.Router();
 
 const modifyLink = require('../services/modifyLink');
 
-router.get('/', async (req, res, next) => {
-    const url = urlencode.decode(req.query.url);
-    if (!url) return res.status(400).end('URL을 파라미터에 포함해야 합니다.');
-    if (!isUrl(url)) return res.status(400).end('잘못된 형식의 URL입니다.');
-
-    delete req.headers.host;
-    delete req.headers.origin;
-    const result = await axios.get(url, {
-        headers: {
-            'user-agent': req.headers['user-agent'],
-            'accept': '*/*',
-            'content-type': 'application/json',
-        }
-    });
-    const html = result.data;
-    if (!html) return res.status(404).send('HTML을 받아오지 못했습니다.');
-
-    const modified_html = modifyLink(url, html);
-    return res.send(modified_html);
+router.get('/', async (req, res) => {
+    try {
+        const url = urlencode.decode(req.query.url);
+        if (!url) return res.status(400).end('URL을 파라미터에 포함해야 합니다.');
+        if (!isUrl(url)) return res.status(400).end('잘못된 형식의 URL입니다.');
+        console.log(req.headers);
+        const result = await axios.get(url, {
+            headers: {
+                connection: 'keep-alive',
+                'user-agent': req.headers['user-agent'],
+                'accept': '*/*',
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        });
+        const html = result.data;
+        if (!html) return res.status(404).end('HTML을 받아오지 못했습니다.');
+    
+        const modified_html = modifyLink(url, html);
+        return res.end(modified_html);
+    } catch (e) {
+        return res.status(500).end('알 수 없는 오류가 발생했습니다.');
+    }
 });
 
 module.exports = router;

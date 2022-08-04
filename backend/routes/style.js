515e62383ba1fbe58a9ceba7b522601b63cfeeef
yeonji <view7186@naver.com>
2022-08-04 13:45:36 +0900

    Refactoring - switch/case 구문 삭제

--- backend/routes/style.js
+++ backend/routes/style.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-const dark = require('../services/colorDark');
-const weak = require('../services/colorWeakness');
-const cont = require('../services/colorHighContrast');
 const css_manager = require('../services/manageStyleSheet');
 const example_html_data = fs.readFileSync('data/sampleHTML', 'utf8');
 const no_url_html_data = fs.readFileSync('data/noUrlHTML', 'utf-8');
 const internal_selector = '#body';
+const color_style = new Map([
+    ['dark', require('../services/colorDark')], 
+    ['color_weakness' , require('../services/colorWeakness')],
+    ['high_contrast' , require('../services/colorHighContrast')]
+])
 
 // 색상 변경 모드 API
 
@@ -25,13 +27,9 @@ router.post('/color', (req, res) => {
     const style_name = req.body.style_change;
     let html_data = req.body.html_data || no_url_html_data;
     let style = null;
-    let color_mode = null;
     if(style_name == 'original' && url != undefined) { res.redirect('/?url='+url); }
-    switch(style_name) {
-        case 'dark':            color_mode = dark; break;
-        case 'color_weakness':  color_mode = weak; break;
-        case 'high_contrast':   color_mode = cont; break;
-    }
+    const color_mode = color_style.get(style_name);
+
     html_data = css_manager.delStyle(html_data, style_category);
     html_data = color_mode.getRenderedHtml(html_data);
     style = css_manager.makeStyle(internal_selector, color_mode.setting, color_mode.strict_mode)
